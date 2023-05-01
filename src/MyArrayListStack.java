033f6f2d85b8bf744ab7020d2e881d0133682aa9
TonyMontana143 <221074@astanait.edu.kz>
2023-05-01 20:59:02 +0600

    task have done

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -2,35 +2,35 @@ import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 public class MyArrayListStack<T> {
-    private ArrayList<T> stackList;
+    private ArrayList<T> stackList; // declare an instance variable of type ArrayList
 
     public MyArrayListStack() {
-        stackList = new ArrayList<T>();
+        stackList = new ArrayList<T>(); // initialize the instance variable in the constructor
     }
 
     public void push(T item) {
-        stackList.add(item);
+        stackList.add(item); // add an element to the top of the stack
     }
 
     public T pop() {
         if (stackList.isEmpty()) {
-            throw new EmptyStackException();
+            throw new EmptyStackException(); // throw an EmptyStackException if the stack is empty
         }
-        return stackList.remove(stackList.size() - 1);
+        return stackList.remove(stackList.size() - 1); // remove and return the top element of the stack
     }
 
     public T peek() {
         if (stackList.isEmpty()) {
-            throw new EmptyStackException();
+            throw new EmptyStackException(); // throw an EmptyStackException if the stack is empty
         }
-        return stackList.get(stackList.size() - 1);
+        return stackList.get(stackList.size() - 1); // return the top element of the stack without removing it
     }
 
     public boolean isEmpty() {
-        return stackList.isEmpty();
+        return stackList.isEmpty(); // return true if the stack is empty or false if it is not
     }
 
     public int size() {
-        return stackList.size();
+        return stackList.size(); // return the number of elements in the stack
     }
 }
